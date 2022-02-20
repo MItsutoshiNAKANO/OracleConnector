6f8d70ce2a663d6f2f3a1f22907385334b7d2ba4
Mitsutoshi Nakano <bkbin005@rinku.zaq.ne.jp>
2022-02-20 10:51:12 +0900

    wrote

--- OracleConnector/Form1.Designer.cs
+++ OracleConnector/Form1.Designer.cs
@@ -28,13 +28,119 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ConnectionStringTextBox = new System.Windows.Forms.TextBox();
+            this.ConnectButton = new System.Windows.Forms.Button();
+            this.ResultLabel = new System.Windows.Forms.Label();
+            this.CloseButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.QueryTextBox = new System.Windows.Forms.TextBox();
+            this.ExecuteButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Connection String";
+            // 
+            // ConnectionStringTextBox
+            // 
+            this.ConnectionStringTextBox.Location = new System.Drawing.Point(148, 7);
+            this.ConnectionStringTextBox.Name = "ConnectionStringTextBox";
+            this.ConnectionStringTextBox.Size = new System.Drawing.Size(640, 25);
+            this.ConnectionStringTextBox.TabIndex = 1;
+            this.ConnectionStringTextBox.Text = "User ID=system;Password=Oracle19;Data Source=SampleDataSource";
+            // 
+            // ConnectButton
+            // 
+            this.ConnectButton.Location = new System.Drawing.Point(601, 38);
+            this.ConnectButton.Name = "ConnectButton";
+            this.ConnectButton.Size = new System.Drawing.Size(90, 35);
+            this.ConnectButton.TabIndex = 2;
+            this.ConnectButton.Text = "Connect";
+            this.ConnectButton.UseVisualStyleBackColor = true;
+            this.ConnectButton.Click += new System.EventHandler(this.ConnectButton_Click);
+            // 
+            // ResultLabel
+            // 
+            this.ResultLabel.AutoSize = true;
+            this.ResultLabel.Location = new System.Drawing.Point(0, 164);
+            this.ResultLabel.Name = "ResultLabel";
+            this.ResultLabel.Size = new System.Drawing.Size(118, 18);
+            this.ResultLabel.TabIndex = 3;
+            this.ResultLabel.Text = "Not connected";
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.Location = new System.Drawing.Point(704, 40);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(83, 32);
+            this.CloseButton.TabIndex = 4;
+            this.CloseButton.Text = "Close";
+            this.CloseButton.UseVisualStyleBackColor = true;
+            this.CloseButton.Click += new System.EventHandler(this.CloseButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(81, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 18);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Query";
+            // 
+            // QueryTextBox
+            // 
+            this.QueryTextBox.Location = new System.Drawing.Point(141, 81);
+            this.QueryTextBox.Name = "QueryTextBox";
+            this.QueryTextBox.Size = new System.Drawing.Size(647, 25);
+            this.QueryTextBox.TabIndex = 6;
+            this.QueryTextBox.Text = "SELECT * FROM dual";
+            // 
+            // ExecuteButton
+            // 
+            this.ExecuteButton.Location = new System.Drawing.Point(601, 112);
+            this.ExecuteButton.Name = "ExecuteButton";
+            this.ExecuteButton.Size = new System.Drawing.Size(178, 36);
+            this.ExecuteButton.TabIndex = 7;
+            this.ExecuteButton.Text = "Execute";
+            this.ExecuteButton.UseVisualStyleBackColor = true;
+            this.ExecuteButton.Click += new System.EventHandler(this.ExecuteButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ExecuteButton);
+            this.Controls.Add(this.QueryTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.CloseButton);
+            this.Controls.Add(this.ResultLabel);
+            this.Controls.Add(this.ConnectButton);
+            this.Controls.Add(this.ConnectionStringTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox ConnectionStringTextBox;
+        private System.Windows.Forms.Button ConnectButton;
+        private System.Windows.Forms.Label ResultLabel;
+        private System.Windows.Forms.Button CloseButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox QueryTextBox;
+        private System.Windows.Forms.Button ExecuteButton;
     }
 }
 
--- OracleConnector/Form1.cs
+++ OracleConnector/Form1.cs
@@ -1,20 +1,69 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Data.Common;
 using System.Windows.Forms;
 
 namespace OracleConnector
 {
     public partial class Form1 : Form
     {
+        private DbConnection connection;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void CloseConnection()
+        {
+            if (connection == null)
+            {
+                return;
+            }
+            connection.Dispose();
+            connection = null;
+        }
+
+        private void ConnectButton_Click(object sender, EventArgs e)
+        {
+            CloseConnection();
+            connection = new OracleConnection
+            {
+                ConnectionString = ConnectionStringTextBox.Text
+            };
+            connection.Open();
+            ResultLabel.Text = "connected";
+        }
+
+        private void ExecuteButton_Click(object sender, EventArgs e)
+        {
+            if (connection == null)
+            {
+                ResultLabel.Text = "Please connect first!";
+                return;
+            }
+            using (DbCommand cmd = connection.CreateCommand())
+            {
+                cmd.CommandText = QueryTextBox.Text;
+                using (DbDataReader reader = cmd.ExecuteReader())
+                {
+                    ResultLabel.Text = "Results:";
+                    while (reader.Read())
+                    {
+                        for (int i = 0; i < reader.FieldCount; ++i)
+                        {
+                            ResultLabel.Text += String.Format((i == 0) ? "\n{0}" : ", {0}", reader[i]);
+                        }
+
+                    }
+                }
+            }
+        }
+
+        private void CloseButton_Click(object sender, EventArgs e)
+        {
+            CloseConnection();
+            ResultLabel.Text = "Not Connected";
         }
     }
 }
